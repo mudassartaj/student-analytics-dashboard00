1e9064bae0f77895cd54182bf00d9c77958c37be
mudassartaj <mudassartaj44@gmail.com>
2026-06-09 22:32:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ h1,h2,h3 {
 # ---------------- LOAD DATA ----------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/StudentsPerformance.csv")
-
+    return pd.read_csv("/mount/src/student-analytics-dashboard00/data/StudentsPerformance.csv")
 df = load_data()
 
 # ---------------- FEATURE ENGINEERING ----------------
